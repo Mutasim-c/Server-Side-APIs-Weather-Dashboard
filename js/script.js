bd708552596d08f220b7395dd1e2b7f73b806bed
mutasim chaudhary <mutasim.chaud@gmail.com>
2023-11-09 15:21:40 +0000

    bootstrap link added and some styling

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=7c3c28474c8587a6386ddc13bfd31f56';
+var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=7c3c28474c8587a6386ddc13bfd31f56&units=metric';
 var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 var list = document.getElementsByTagName("H1")[0];
-var requestUrl2 = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=7c3c28474c8587a6386ddc13bfd31f56'
-var requestUrl3 = 'api.openweathermap.org/data/2.5/forecast?q=London,us&appid=7c3c28474c8587a6386ddc13bfd31f56'
-var requestUrl4 = 'api.openweathermap.org/data/2.5/forecast?q=München,DE&appid=7c3c28474c8587a6386ddc13bfd31f56'
+var requestUrl2 = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=7c3c28474c8587a6386ddc13bfd31f56&units=metric'
+var requestUrl3 = 'api.openweathermap.org/data/2.5/forecast?q=London,us&appid=7c3c28474c8587a6386ddc13bfd31f56&units=metric'
+var requestUrl4 = 'api.openweathermap.org/data/2.5/forecast?q=München,DE&appid=7c3c28474c8587a6386ddc13bfd31f56&units=metric'
+
 function getApi(requestUrlArg) {
+  var temp
   fetch(proxyUrl + requestUrlArg).then(function (response) {
     //console.log(response);
     if (response.status === 200) {
@@ -14,10 +16,14 @@ function getApi(requestUrlArg) {
   })
   .then(function (data) {
     console.log(data);
+    //console.log(data.cnt);
+    temp = data.list[0].main.temp;
+    console.log(temp);
   });
 }
 
 //getApi(requestUrl);
-getApi(requestUrl2);
-//getApi(requestUrl3);
+//getApi(requestUrl2);
+getApi(requestUrl3);
+//console.log(london);
 //getApi(requestUrl4);
