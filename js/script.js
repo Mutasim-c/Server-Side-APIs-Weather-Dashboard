d69ebfcec07c5837b7e813669d6a8ff5e7c2fddc
mutasim chaudhary <mutasim.chaud@gmail.com>
2023-11-09 21:57:16 +0000

    added some styling

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-// var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=7c3c28474c8587a6386ddc13bfd31f56&units=metric';
 var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-var header = document.getElementsByTagName("H1")[0];
-// var requestUrl2 = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=7c3c28474c8587a6386ddc13bfd31f56&units=metric'
-// var requestUrl3 = 'api.openweathermap.org/data/2.5/forecast?q=London,us&appid=7c3c28474c8587a6386ddc13bfd31f56&units=metric'
-// var requestUrl4 = 'api.openweathermap.org/data/2.5/forecast?q=München,DE&appid=7c3c28474c8587a6386ddc13bfd31f56&units=metric'
 var day0 = document.getElementById('0')
 var day1 = document.getElementById('1')
 var day2 = document.getElementById('2')
@@ -11,9 +6,7 @@ var day3 = document.getElementById('3')
 var day4 = document.getElementById('4')
 var day5 = document.getElementById('5')
 var apiKey = "7c3c28474c8587a6386ddc13bfd31f56"
-// var city;
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-//console.log(test);
+
 function weatherData(data , day , whichDay){
   var name = data.city.name;
   var date = data.list[day].dt_txt;
@@ -30,20 +23,15 @@ function weatherData(data , day , whichDay){
 }
 function getApi(requestUrlArg) {
   fetch(proxyUrl + requestUrlArg).then(function (response) {
-    //console.log(response);
     if (response.status === 200) {
-      header.textContent = response.status;
       bool = true;
     }else{
       bool = false
-      console.log('hello');
       return;
     }
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
-    //console.log(data.cnt);
     var number = 0
     weatherData(data , 0, day0);
     weatherData(data , 8, day1);
@@ -61,12 +49,6 @@ function getApi(requestUrlArg) {
 
   });
 }
-
-//getApi(requestUrl);
-//getApi(requestUrl2);
-//getApi(requestUrl3);
-//console.log(london);
-//getApi(requestUrl4);
 var bool = false;
 var search = document.getElementById('search');
 var input = document.getElementById('name-input');
@@ -87,13 +69,13 @@ function showResponse(event) {
   event.preventDefault();
   var city2 = input.value;
   getApi('api.openweathermap.org/data/2.5/forecast?q='+city2+'&appid='+apiKey + '&units=metric');
+  input.value = "";
 }
   
 // Add listener to submit element
 search.addEventListener("click", showResponse);
 
 function history(event) {
-
   var place = localStorage.getItem(event.target.textContent);
   getApi('api.openweathermap.org/data/2.5/forecast?q='+place+'&appid='+apiKey + '&units=metric');
 }
