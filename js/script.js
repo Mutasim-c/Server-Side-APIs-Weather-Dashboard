1356ffb1ca9de228e1f5287853d5c9d46ef1f7d5
mutasim chaudhary <mutasim.chaud@gmail.com>
2023-11-09 22:02:15 +0000

    added comments

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+var proxyUrl = 'https://cors-anywhere.herokuapp.com/';//gets accross cors
 var day0 = document.getElementById('0')
 var day1 = document.getElementById('1')
 var day2 = document.getElementById('2')
 var day3 = document.getElementById('3')
 var day4 = document.getElementById('4')
-var day5 = document.getElementById('5')
+var day5 = document.getElementById('5')//gets all the elements where the weather data will go
 var apiKey = "7c3c28474c8587a6386ddc13bfd31f56"
 
 function weatherData(data , day , whichDay){
@@ -20,7 +20,7 @@ function weatherData(data , day , whichDay){
   whichDay.children[2].textContent = "Temp: " + temp +"°C";
   whichDay.children[3].textContent = "Wind Speed: " + windSpeed +"m/s";
   whichDay.children[4].textContent = "Humidity: " + humidity +"%";
-}
+}//prints all the weather data to the html
 function getApi(requestUrlArg) {
   fetch(proxyUrl + requestUrlArg).then(function (response) {
     if (response.status === 200) {
@@ -28,7 +28,7 @@ function getApi(requestUrlArg) {
     }else{
       bool = false
       return;
-    }
+    }//fethes api
     return response.json();
   })
   .then(function (data) {
@@ -38,14 +38,14 @@ function getApi(requestUrlArg) {
     weatherData(data , 16, day2);
     weatherData(data , 24, day3);
     weatherData(data , 32, day4);
-    weatherData(data , 39, day5);
+    weatherData(data , 39, day5);//gets the info for each day and use function to display to html
     if(bool && !ifVisited(data.city.name)){
       var newList = document.createElement("li");
       newList.textContent = data.city.name;
       visitedPlaces.push(data.city.name);
       list.appendChild(newList);
       localStorage.setItem(""+data.city.name, data.city.name );
-    }
+    }//checks if the place has already been searched or if its real and it will not add it to search history
 
   });
 }
@@ -62,7 +62,7 @@ function ifVisited(current){
     }
   }
   return hasVisited;
-}
+}//checks if a place has already been visited
 
 function showResponse(event) {
   // Prevent default action
@@ -70,7 +70,7 @@ function showResponse(event) {
   var city2 = input.value;
   getApi('api.openweathermap.org/data/2.5/forecast?q='+city2+'&appid='+apiKey + '&units=metric');
   input.value = "";
-}
+}//when button pressed search for api with the input from user
   
 // Add listener to submit element
 search.addEventListener("click", showResponse);
@@ -78,6 +78,6 @@ search.addEventListener("click", showResponse);
 function history(event) {
   var place = localStorage.getItem(event.target.textContent);
   getApi('api.openweathermap.org/data/2.5/forecast?q='+place+'&appid='+apiKey + '&units=metric');
-}
+}//if search history clicked it will get it and print it to page
 list.addEventListener("click", history)
 
