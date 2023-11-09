96460cdaf69c6f19e50c4b92a3212fd5e0bf98fe
mutasim chaudhary <mutasim.chaud@gmail.com>
2023-11-09 22:28:29 +0000

    local storage data displayed on page startup

--- js/script.js
+++ js/script.js
@@ -6,6 +6,27 @@ var day3 = document.getElementById('3')
 var day4 = document.getElementById('4')
 var day5 = document.getElementById('5')//gets all the elements where the weather data will go
 var apiKey = "7c3c28474c8587a6386ddc13bfd31f56"
+var bool = false;
+var search = document.getElementById('search');
+var input = document.getElementById('name-input');
+var list = document.getElementById('list')
+var visitedPlaces = []
+
+renderVisitedPlaces();
+
+function renderVisitedPlaces(){
+  var storedPlaces = JSON.parse(localStorage.getItem("visited"));
+  if (!storedPlaces) {
+    return;
+  }
+  console.log(storedPlaces)
+  visitedPlaces = storedPlaces;
+  for (var i = 0; i < visitedPlaces.length; i++) { 
+    var newList = document.createElement("li");
+    newList.textContent = visitedPlaces[i];
+    list.appendChild(newList);
+  }
+}
 
 function weatherData(data , day , whichDay){
   var name = data.city.name;
@@ -44,16 +65,12 @@ function getApi(requestUrlArg) {
       newList.textContent = data.city.name;
       visitedPlaces.push(data.city.name);
       list.appendChild(newList);
-      localStorage.setItem(""+data.city.name, data.city.name );
+      //localStorage.setItem(""+data.city.name, data.city.name );
+      localStorage.setItem("visited", JSON.stringify(visitedPlaces));
     }//checks if the place has already been searched or if its real and it will not add it to search history
 
   });
 }
-var bool = false;
-var search = document.getElementById('search');
-var input = document.getElementById('name-input');
-var list = document.getElementById('list')
-var visitedPlaces = []
 function ifVisited(current){
   hasVisited = false;
   for (var i = 0; i < visitedPlaces.length; i++) { 
@@ -76,8 +93,9 @@ function showResponse(event) {
 search.addEventListener("click", showResponse);
 
 function history(event) {
-  var place = localStorage.getItem(event.target.textContent);
+  var place = event.target.textContent
   getApi('api.openweathermap.org/data/2.5/forecast?q='+place+'&appid='+apiKey + '&units=metric');
 }//if search history clicked it will get it and print it to page
 list.addEventListener("click", history)
+
 
